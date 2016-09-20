b45a1e1ef575273aa20f0373dd9d84bf2c4f9c81
Miguel Puig <miguelpuig@loveflutter.com>
2016-09-20 07:56:05 +0100

    rename jade to pug views

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var pug = require('pug');
 
 // New Code
 var mongo = require('mongodb');
@@ -18,7 +19,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
